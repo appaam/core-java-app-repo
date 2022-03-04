a85070bee4671ce5e6ad7dfc849b6be67dc4683a
preethi <preethiyadav676@gmail.com>
2022-03-04 14:50:21 +0530

    Merge branch 'multithread_branch' of https://github.com/appaam/core-java-app-repo.git

--- java-multithread-app/src/com/sapient/aem/ui/MyThreadDemo.java
+++ java-multithread-app/src/com/sapient/aem/ui/MyThreadDemo.java
@@ -5,9 +5,11 @@ import com.sapient.aem.service.MyThread;
 public class MyThreadDemo {
 
 	public static void main(String[] args) {
+		System.out.println(System.currentTimeMillis());
 		System.out.println(Thread.currentThread());
 		Thread t1= new MyThread("worker-1");
 		t1.start();//invokes run() method
+		System.out.println(System.currentTimeMillis());
 		System.out.println("End of main() method");
 
 	}
